05489b58d848477cdfb2269eae9e7f6d37c8152b
Agbonrofo Oselumese <113614713+oselumeseagbonrofo@users.noreply.github.com>
2025-10-13 17:15:52 +0100

    Updated sponsors section

--- components/SponsorsPartners.tsx
+++ components/SponsorsPartners.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 
 export default function SponsorsPartners() {
   return (
-    <div className="mx-auto flex max-w-screen-lg px-8 pb-16 pt-4 sm:px-4 md:px-0 lg:pb-0">
+    <div className="mx-auto flex max-w-screen-lg px-8 pb-16 pt-4 m-5 sm:px-4 md:px-0 lg:pb-0">
       <div className="m-auto">
         <motion.p 
           className="mb-4 text-center font-pp-mondwest text-3xl sm:text-4xl xl:text-5xl"
@@ -14,7 +14,7 @@ export default function SponsorsPartners() {
           viewport={{ once: true }}
           transition={{ duration: 0.6 }}
         >
-          Sponsors & Partners
+          Sponsors
         </motion.p>
         <motion.p 
           className="text-center text-base text-black sm:text-xl xl:text-2xl"
@@ -23,48 +23,10 @@ export default function SponsorsPartners() {
           viewport={{ once: true }}
           transition={{ duration: 0.6, delay: 0.2 }}
         >
-          A round of applause for our sponsors, who make all of this possible.
+          Big thanks to the sponsors who make this hackathon possible!
         </motion.p>
-        <div className="mt-16 flex flex-row justify-center gap-4">
-          <a
-            href="https://ticpau.vercel.app"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="relative z-50 m-0 -mt-1.5 h-[80px] w-[160px] p-0 sm:h-[100px] sm:w-[200px] xl:h-[125px] xl:w-[250px]"
-          >
-            <Image
-              src="/images/tic_logo.png"
-              alt="Tech Innovation Club Logo"
-              fill
-            />
-          </a>
-          <a
-            href="https://pau.edu.ng/"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="relative z-50 m-0 h-[56px] w-[160px] p-0 sm:h-[70px] sm:w-[200px] xl:h-[87.5px] xl:w-[250px]"
-          >
-            <Image
-              src="/images/pau_logo.png"
-              alt="Pan-Atlantic University Logo"
-              fill
-            />
-          </a>
-        </div>
         <div className="mt-12 grid grid-cols-2 items-center justify-items-center gap-8 lg:grid-cols-2 lg:gap-10">
-          <a
-            href="https://www.servicenow.com/"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="relative z-50 m-0 -mt-1.5 h-[150px] w-full p-0"
-          >
-            <Image
-              src="/images/sponsors/servicenow_uni_meetup.png"
-              alt="Servicenow University Meetup Nigeria Logo"
-              fill
-            />
-          </a>
-          <a
+           <a
             href="https://www.zenithbank.com/"
             rel="noopener noreferrer"
             target="_blank"
@@ -74,6 +36,18 @@ export default function SponsorsPartners() {
               src="/images/sponsors/zenith.png"
               alt="Zenith Bank Logo"
               className="object-contain"
+              fill
+            />
+          </a>
+          <a
+            href="https://www.servicenow.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="relative z-50 m-0 -mt-1.5 h-[150px] w-full p-0"
+          >
+            <Image
+              src="/images/sponsors/servicenow_uni_meetup.png"
+              alt="Servicenow University Meetup Nigeria Logo"
               fill
             />
           </a>
